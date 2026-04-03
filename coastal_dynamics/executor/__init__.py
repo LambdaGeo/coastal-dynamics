35b98f22bf2ffbd7da65d7a01b698b7f36f02511
Sergio Souza Costa <sergio.costa@ufma.br>
2026-04-03 06:22:39 -0300

    update

---
+++ coastal_dynamics/executor/__init__.py
@@ -0,0 +1,22 @@
+# coastal_dynamics/executor/__init__.py
+
+from .coastal_raster_executor import CoastalRasterExecutor
+from .coastal_vector_executor import CoastalVectorExecutor
+from .coastal_validation_executor import CoastalValidationExecutor
+
+# A variável mágica __all__ define exatamente o que é exportado
+# quando alguém faz `from coastal_dynamics.executor import *`
+__all__ = [
+    "CoastalRasterExecutor",
+    "CoastalVectorExecutor",
+    "CoastalValidationExecutor",
+    "EXECUTOR_REGISTRY", # Exportando o registro também
+]
+
+# BÔNUS PARA A API/WORKER:
+# Um dicionário que mapeia a string do request (JSON) para a Classe real
+EXECUTOR_REGISTRY = {
+    CoastalRasterExecutor.name: CoastalRasterExecutor,
+    CoastalVectorExecutor.name: CoastalVectorExecutor,
+    CoastalValidationExecutor.name: CoastalValidationExecutor,
+}
